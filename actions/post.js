9de8780860d6576ba06a223b955ace23dea9de87
Ashish Yadav <yashish9990@gmail.com>
2026-06-29 00:28:15 +0530

    Revalidating the data to avoid Caching Problems.

--- actions/post.js
+++ actions/post.js
@@ -3,6 +3,7 @@
 import { redirect } from "next/navigation";
 import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudinary";
+import { revalidatePath } from "next/cache";
 
 export async function createPost(prevState, formData) {
   "use server";
@@ -54,5 +55,7 @@ export async function createPost(prevState, formData) {
 }
 
 export async function togglePostLikeStatus(postId) {
-  updatePostLikeStatus(postId, 2);
+  await updatePostLikeStatus(postId, 2);
+//   revalidatePath('/feed');
+  revalidatePath('/','layout');
 }
