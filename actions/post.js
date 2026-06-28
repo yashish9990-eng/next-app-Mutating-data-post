9438000b445befc2fa0ab7b826206c470270eb89
Ashish Yadav <yashish9990@gmail.com>
2026-06-29 00:00:41 +0530

    Uploading and Storing Images.

--- actions/post.js
+++ actions/post.js
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { storePost } from "@/lib/posts";
+import { uploadImage } from "@/lib/cloudinary";
 
 export async function createPost(prevState, formData) {
   "use server";
@@ -29,8 +30,21 @@ export async function createPost(prevState, formData) {
 
   console.log(title, image, content);
 
+  let imageUrl;
+  try {
+    imageUrl = await uploadImage(image);
+  } catch (error) {
+    return {
+      errors: [
+        error instanceof Error
+          ? error.message
+          : "Image Upload Failed, Post was not Created. Please Try Again Later.",
+      ],
+    };
+  }
+
   await storePost({
-    imageUrl: "",
+    imageUrl: imageUrl,
     title,
     content,
     userId: 1,
